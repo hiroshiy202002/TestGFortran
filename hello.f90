215558b0002f3677dd760f54e94e3ddfdd2ad4ce
hiroshiy202002 <hiroshiy90@outlook.com>
2020-02-13 15:55:05 +0900

    mod hello.f90

--- hello.f90
+++ hello.f90
@@ -1,5 +1,24 @@
 program hello
    implicit none
-   write(*,*) "hello"
+   integer, parameter:: n = 4
+   double precision, dimension(0:n):: xi
+   integer i
+   write(*,*) "hello!!!"
+   call gausslobattopoints( n, xi )
+   do i = 0, n
+      write(*,*) xi(i)
+   end do
    stop
 end program hello
+
+subroutine gausslobattopoints( n, xi )
+   implicit none
+   integer n
+   double precision, dimension(0:n):: xi
+   double precision, parameter:: pi = dacos(-1.0d0)
+   integer i
+   do i = 0, n
+      xi(i) = dcos(pi*dble(i)/dble(n))
+   end do
+   return
+end subroutine gausslobattopoints
